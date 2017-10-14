8f14d5b6e1196322b1134e010905c992a01de2df
Minh Tam <minhtam.seta@gmail.com>
2017-10-14 12:01:36 +0700

    Add debug task

---
+++ HangFireWebExample/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Owin;
+using System.Diagnostics;
+
+[assembly: OwinStartup(typeof(HangFireWebExample.Startup))]
+namespace HangFireWebExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangfireContext");
+
+            app.UseHangfireDashboard("/WebJobsDashboard");
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate(
+                () => Debug.WriteLine("Minutely Job"), Cron.Minutely);
+        }
+    }
+}
